4339927ed9c6d107ab1406cca97f8802eaa4e126
jchen2021 <57500286+jchen2021@users.noreply.github.com>
2020-09-24 10:33:12 -0400

    m

---
+++ app.js
@@ -0,0 +1,20 @@
+/* console.log("Hello my name is " + " JY")
+
+console.log('Hello it\'s me Jy'); */
+
+/*console.log("Hello it's me JY");*/
+
+/*const name = "JYU"; //typeof can check why type of the variable is
+const age = 24;*/
+/*console.log(`Hello it's me and my name is ${name}`);*/
+/* 
+console.log("Hello it's me " + name " and my age is " + age + “ and i want sleep");
+
+console.log(`Hello it's me ${name} and my name is ${age}`); */
+
+/*const combined = name + age;
+console.log(combined); //JYU24 - make it a string*/
+
+
+
+
