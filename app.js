88cc36168c5634b68e2702f6314bfdfbbb00e1aa
jchen2021 <57500286+jchen2021@users.noreply.github.com>
2020-09-24 10:56:21 -0400

    ?

---
+++ app.js
@@ -0,0 +1,66 @@
+/* console.log("Hello my name is " + " JY")
+
+console.log('Hello it\'s me Jy'); */
+
+/*console.log("Hello it's me JY");*/
+
+/*const name = "JYU"; //typeof can check why type of the variable is
+const age = 24;*/
+/*console.log(`Hello it's me and my name is ${name}`);*/
+/* 
+console.log("Hello it's me " + name " and my age is " + age + “ and i want sleep");
+
+console.log(`Hello it's me ${name} and my name is ${age}`); */
+
+/*const combined = name + age;
+console.log(combined); //JYU24 - make it a string*/
+
+/* const name = "Jiayu Chen";
+
+function toUpper(text){
+    const upperCaded = text.toUpperCase();
+    console.log(upperCased);
+}
+
+toUpper(name)；
+
+ */
+
+/*  const toUpper = function(){
+     asdashed
+ }
+
+const toUpper = () =>{
+    asdashed
+} */
+
+/* const num1 = 100;
+const num2 = 50;
+let val;
+
+//simple math with numbers
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+//Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-5);
+val = Math.pow(8, 2);
+val = Math.min(2,5,14,37593);
+val = Math.max(3,345,656,2321,5);
+val = Math.random();
+
+val = Math.random() * 20; //between 0-19 with desmos
+val = Math.floor(Math.random() * 20 +1); //whole number between 0-20
+
+
+console.log(val); */
+
